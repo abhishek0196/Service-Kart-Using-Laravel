c505ca06a86d11d7de895316beafeb180dea033d
abhishek0196 <abhiagarwal96@gmail.com>
2018-09-13 11:57:59 +0530

    added category view

--- app/Http/Controllers/CustomerAuth/Orders/OfficeServiceController.php
+++ app/Http/Controllers/CustomerAuth/Orders/OfficeServiceController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\CustomerAuth\Orders;
 use App\Product;
 
+use App\Serviceprovider;
+
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 use DB;
 use Auth;
+use Mail;
 
 class OfficeServiceController extends Controller
 {
@@ -42,10 +45,18 @@ class OfficeServiceController extends Controller
      */
     public function store(Request $request)
     {
+        $google_code = $request->input('google_code');
        
         DB::table('orders')->insert(
-            ['customer-id' => Auth::guard('customer')->user()->id, 'serviceprovider-id' => $request->input('psid'),'service-id' => $request->input('pid') ]
+            ['customer-id' => Auth::guard('customer')->user()->id, 'serviceprovider-id' => $request->input('psid'),'service-id' => $request->input('pid'),'google_code' => $google_code ]
         );
+
+        $data = array('name'=>Auth::guard('customer')->user()->name,'pname'=>$request->input('pname'),'pDesc'=>$request->input('pdesc'));
+        Mail::send('mail.mail', $data, function($message) {
+           $message->to(Auth::guard('customer')->user()->email, Auth::guard('customer')->user()->name)->subject
+              ('Service Kart');
+           
+        });
         return redirect('/customer/home');//->with('success', 'Post Removed');
     }
 
@@ -58,41 +69,25 @@ class OfficeServiceController extends Controller
     public function show($id)
     {
         $product = Product::find($id);
+        
+        $query = Serviceprovider::find($product->serviceprovider_id);
+        
+     
+        return view('customer.order.Office.show')->with('product',$product)->with('serviceprovider',$query);
+    }
+    public function categoryView()
+    {
+        $query = DB::select('select * from product_tags where pcid = ?', [1]);
 
-        return view('customer.order.Office.show')->with('product',$product);
+        return view('customer.order.Office.category')->with('data' ,$query);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    public function  productView($id)
     {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $query = DB::select('select * from products  where id in (select product_id from product_product_tag where product_tag_id = ? )',[$id]);
+        return view('customer.order.Office.index')->with('cproducts',$query);;
+        
+        
+        
     }
 }
